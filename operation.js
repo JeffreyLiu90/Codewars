4f64cbb2155401496b076f94ecf8fe3a55b17789
Jeffrey <jeffrey.liu90@gmail.com>
2020-06-01 23:48:41 -0400

    added switch case method solution

--- operation.js
+++ operation.js
@@ -4,3 +4,20 @@ function basicOp(operation, value1, value2) {
 }
 
 console.log(basicOp("+", 4, 7));
+
+//the switch case method
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      return value1 / value2;
+    default:
+      return 0;
+  }
+}
