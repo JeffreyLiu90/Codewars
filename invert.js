ed91f6b1f6c05b619c8a9921d54dc61de2207582
Jeffrey <jeffrey.liu90@gmail.com>
2020-06-13 23:46:28 -0400

    invert and string to numbers exercise

--- invert.js
+++ invert.js
@@ -1,15 +1,12 @@
-// function invert(array) {
-//   if (array.length === 0) {
-//     console.log("[]");
-//   } else {
-//     console.log("not ");
-//   }
-// }
-
 const invert = array =>
   array.length === 0
     ? []
     : array.map(num => (num < 0 ? Math.abs(num) : -Math.abs(num)));
+
+//Online Solutions
+function invert(array) {
+  return array.map(i => 0 - i);
+}
 
 console.log(invert([1, 2, 3, 4, 5])); // == [-1, -2, -3, -4, -5]);
 console.log(invert([1, -2, 3, -4, 5])); // == [-1, 2, -3, 4, -5]);
