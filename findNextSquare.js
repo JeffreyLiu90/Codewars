b34d872c6f2f04baa8e7513f45ef28fa5cc6de51
Jeffrey <jeffrey.liu90@gmail.com>
2020-07-17 15:18:34 -0400

    Find the next square kata and printer errors kata from codewars kyu 7

---
+++ findNextSquare.js
@@ -0,0 +1,25 @@
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is positive.
+
+// Examples:
+
+// findNextSquare(121) --> returns 144
+// findNextSquare(625) --> returns 676
+// findNextSquare(114) --> returns -1 since 114 is not a perfect
+
+const findNextSquare = num => {
+  let square = Math.sqrt(num);
+
+  return Number.isInteger(square) ? Math.pow(square + 1, 2) : -1;
+};
+
+//Other Solution
+const findNextSquare = num =>
+  Number.isInteger(Math.sqrt(num)) ? Math.pow(Math.sqrt(num) + 1, 2) : -1;
+
+console.log(findNextSquare(121));
+console.log(findNextSquare(114));
+// console.log(findNextSquare(625))
