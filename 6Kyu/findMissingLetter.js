3019b1a7f70a6b3040748a464a1c21b6aba2d6a9
Jeffrey <jeffrey.liu90@gmail.com>
2020-08-30 23:21:03 -0400

    Find missing letter kata

---
+++ 6Kyu/findMissingLetter.js
@@ -0,0 +1,24 @@
+const findMissingLetter = arr => {
+  const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lettersArr = letters.split("");
+
+  const emptArr = [];
+  const answer = [];
+  //index of first letter?
+
+  let startingIndex = lettersArr.indexOf(arr[0]);
+  let endingIndex = startingIndex + arr.length + 1;
+
+  //includes?
+
+  let newArr = lettersArr.slice(startingIndex, endingIndex);
+
+  newArr.map(letter =>
+    arr.includes(letter) ? emptArr.push(letter) : answer.push(letter)
+  );
+  return answer[0];
+};
+
+console.log(findMissingLetter(["a", "b", "d", "e"]));
+
+// Online answers
